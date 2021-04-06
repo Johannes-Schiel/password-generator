efc8348fceb22f44d861f494f9ddf1a07e6a0420
TheMorpheus407 <Cedric.Moessner@gmx.de>
2021-04-06 10:30:08 +0200

    add password generator

--- src/js/generatepassword.js
+++ src/js/generatepassword.js
@@ -1,9 +1,45 @@
-const faker = require('faker');
-
 export class PasswordGenerator {
-
-    getPassword(config) {
-        return Array.from(faker.lorem.word());
+    getRandomNumbers(length) {
+        var array = new Uint8Array(length);
+        var res = new Float32Array(length);
+        window.crypto.getRandomValues(array);
+        for (let i = 0; i < array.length; i++) {
+            res[i] = array[i] / 0xFF;
+        }
+        return res;
     }
 
+    getPassword(config) {
+        var result = [];
+        var characters = this.genChars(config);
+        var charactersLength = characters.length;
+        let randomness = this.getRandomNumbers(config["length"]);
+        for ( let i = 0; i < config["length"]; i++ ) {
+            let char = characters.charAt(Math.floor(randomness[i] * charactersLength));
+            result.push(char);
+        }
+        let res = result.join('');
+        console.log(res);
+        return result;
+    }
+
+    genChars(config) {
+        let chars = "";
+        if (!config["numbers"] && !config["letters"] && !config["specialCharacters"] && !config["lockedSpecialCharacters"]) {
+            return "-";
+        }
+        if (config["numbers"]){
+            chars = chars + "0123456789";
+        }
+        if (config["letters"]){
+            chars = chars + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        }
+        if (config["specialCharacters"]){
+            chars = chars + "!§$%/()´`^°*~#|,:._-€@";
+        }
+        if (config["lockedSpecialCharacters"]){
+            chars = chars + "\\?\"[{\'&>;+}=]< ";
+        }
+        return chars;
+    }
 }
